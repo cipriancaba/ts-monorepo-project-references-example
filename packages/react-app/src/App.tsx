eb6d05aae0a1a0a61b4a36bc3bb05cd0779af9fc
Andreas Brekken <andreas@brekken.com>
2019-12-13 12:22:13 +0800

    Add more nested imports

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { SHARED_CONFIG } from '@mono/shared'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { SHARED_CONFIG, DummyShared } from '@mono/shared';
+import { CompOne } from '@mono/comps';
+import { LegacyComp } from './LegacyComp';
+
+enum Dummy1 {
+  A,
+  B,
+}
+
+const a: Dummy1 = Dummy1.A;
+const b: DummyShared = DummyShared.chair;
 
 const App: React.FC = () => {
   return (
@@ -9,18 +19,17 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>I SEE MONOREPOS</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
         <p>Is shared: {SHARED_CONFIG.SHARED}</p>
+        <p>
+          <CompOne />
+        </p>
+        <LegacyComp />
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,14 @@
+export enum DummyShared {
+  chair,
+  table,
+}
+
 export const SHARED_CONFIG = {
   DUMMY: true,
   SHARED: 'of course',
-  MONO: 'REPO'
+  MONO: 'REPO',
+};
+
+export function sum(a: number, b: number): number {
+  return a + b;
 }
