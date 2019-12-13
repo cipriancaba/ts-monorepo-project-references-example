d1344f9501c1d48293c5218b7226c1d1df41be51
Andreas Brekken <andreas@brekken.com>
2019-12-13 14:09:28 +0800

    Add tests for app (broken)

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,5 +1,15 @@
-import { SHARED_CONFIG } from '@mono/shared'
+import { SHARED_CONFIG } from '@mono/shared';
 
-const APP = true
+const APP = true;
 
-console.log(SHARED_CONFIG)
+console.log(SHARED_CONFIG);
+
+export function times(a: number, b: number): number {
+  let result = 0;
+
+  for (let i = 0; i < b; i++) {
+    result = result + a;
+  }
+
+  return result;
+}
